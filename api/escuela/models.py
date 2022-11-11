b40c27fc7788b73d83d6b38b4e542071fe437550
Alicia Klopkova <aaportela73@gmail.com>
2022-11-10 19:04:16 -0500

    Agregar modelos

--- api/escuela/models.py
+++ api/escuela/models.py
@@ -1,3 +1,112 @@
 from django.db import models
 
-# Create your models here.
+
+class Grado(models.Model):
+    grado_choice = [
+        ('7', '7mo'),
+        ('8', '8vo'),
+        ('9', '9no')
+    ]
+    nombre = models.CharField(max_length=1, choices=grado_choice, primary_key=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Asignatura(models.Model):
+    nombre = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Grupo(models.Model):
+    numero_grupo = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.numero_grupo
+
+
+class Profesor(models.Model):
+    categoria_docente_choices = [
+        ('Profesor Titular', 'Profesor Titular'),
+        ('Profesor Auxiliar', 'Profesor Auxiliar'),
+        ('Asistente', 'Asistente'),
+        ('Instructor', 'Instructor'),
+        ('Instructor Auxiliar', 'Instructor Auxiliar'),
+        ('Auxiliar Técnico de la Docencia', 'Auxiliar Técnico de la Docencia'),
+        ('Profesor de Mérito', 'Profesor de Mérito'),
+        ('Profesor Invitado', 'Profesor Invitado')
+    ]
+    categoria_cientifica_choices = [
+        ('máster', 'máster'),
+        ('doctor', 'doctor'),
+    ]
+    nombre = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField
+    ci = models.CharField(max_length=11, primary_key=True, unique=True)
+    direccion = models.CharField(max_length=200)
+    numero_telefono = models.CharField(max_length=11)
+    categoria_docente = models.CharField(max_length=100, choices=categoria_docente_choices, null=True, blank=True)
+    categoria_cientifica = models.CharField(max_length=100, choices=categoria_cientifica_choices, null=True, blank=True)
+    grupos = models.ManyToManyField(Grupo)
+    asignatura = models.ManyToManyField(Asignatura)
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellidos}'
+
+
+class ProfesorGuia(Profesor):
+    grupo = models.OneToOneField(Grupo, on_delete=models.CASCADE)
+    profesor = models.OneToOneField(Profesor, on_delete=models.CASCADE, related_name='pg_profesor')
+
+
+class Estudiante(models.Model):
+    genero_choice = [
+        ('F', 'Femenino'),
+        ('M', 'Masculino')
+    ]
+
+    nombre = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+    ci = models.CharField(max_length=11, primary_key=True, unique=True)
+    direccion = models.CharField(max_length=200)
+    reparto = models.CharField(max_length=30)
+    municipio = models.CharField(max_length=30)
+    provincia = models.CharField(max_length=30)
+    numero_telefono = models.CharField(max_length=11)
+    genero = models.CharField(max_length=1, choices=genero_choice)
+    edad = models.IntegerField()
+    nombre_apellido_padre = models.CharField(max_length=150)
+    nombre_apellido_madre = models.CharField(max_length=150)
+    grado = models.ForeignKey(Grado, on_delete=models.DO_NOTHING, null=True)
+    grupo = models.ForeignKey(Grupo, on_delete=models.SET_NULL, null=True)
+
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellidos}'
+
+
+class Nota(models.Model):
+    valor = models.IntegerField()
+    tipo = models.CharField(max_length=100)
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    asignatura = models.ForeignKey(Asignatura, on_delete=models.RESTRICT)
+
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=20, primary_key=True, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class ProgramaDeEstudio(models.Model):
+    asignatura = models.ManyToManyField(Asignatura)
+    curso = models.ForeignKey(Curso, on_delete=models.RESTRICT)
+    grado = models.ForeignKey(Grado, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return f'{self.curso} {self.grado}'
