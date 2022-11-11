c71f5267cfcb202b6247ff65aa29bb23fb1ef013
Alicia Klopkova <aaportela73@gmail.com>
2022-11-10 19:03:00 -0500

    Agregar urls

--- api/urls.py
+++ api/urls.py
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from api.escuela import views as escuela_views
+from rest_framework.authtoken import views as authtoken_views
+
+router = routers.DefaultRouter()
+router.register(r'estudiante', escuela_views.EstudianteViewSet)
+router.register(r'profesor', escuela_views.ProfesorViewSet)
+router.register(r'profesorguia', escuela_views.ProfesorGuiaViewSet)
+router.register(r'grupo', escuela_views.GrupoViewSet)
+router.register(r'grado', escuela_views.GradoViewSet)
+router.register(r'asignatura', escuela_views.AsignaturaViewSet)
+router.register(r'nota', escuela_views.NotaViewSet)
+router.register(r'curso', escuela_views.CursoViewSet)
+router.register(r'programadeestudio', escuela_views.ProgramaDeEstudioViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', authtoken_views.obtain_auth_token),
 ]
