3f232563d4c39518d0b72e19b27cbae9857d41be
Abel Cuza <abel.cuza07@gmail.com>
2022-11-20 18:51:17 -0500

    Add object name for every model in the serializer

--- api/escuela/serializers.py
+++ api/escuela/serializers.py
@@ -5,54 +5,103 @@ from .models import Estudiante, Profesor, ProfesorGuia, ProgramaDeEstudio, Curso
 
 
 class EstudianteSerializer(serializers.ModelSerializer):
+    object_name = serializers.SerializerMethodField()
+
+    def get_object_name(self, obj):
+        return obj.__str__()
+
     class Meta:
         model = Estudiante
-        fields = '__all__'
+        fields = ['id', 'nombre', 'primer_apellido', 'segundo_apellido', 'fecha_nacimiento', 'ci', 'direccion',
+                  'numero_telefono', 'reparto', 'municipio', 'provincia', 'genero', 'edad', 'nombre_apellido_padre',
+                  'nombre_apellido_madre', 'grado', 'grupo', 'object_name']
 
 
 class ProfesorSerializer(serializers.ModelSerializer):
+    object_name = serializers.SerializerMethodField()
+
+    def get_object_name(self, obj):
+        return obj.__str__()
+
     class Meta:
         model = Profesor
-        fields = '__all__'
+        fields = ['id', 'nombre', 'primer_apellido', 'segundo_apellido', 'fecha_nacimiento', 'ci', 'direccion',
+                  'numero_telefono', 'reparto', 'municipio', 'provincia', 'genero', 'edad', 'grado_academico',
+                  'categoria_docente', 'grupos', 'asignaturas', 'object_name']
 
 
 class ProfesorGuiaSerializer(serializers.ModelSerializer):
+    object_name = serializers.SerializerMethodField()
+
+    def get_object_name(self, obj):
+        return obj.__str__()
+
     class Meta:
         model = ProfesorGuia
-        fields = ['grupo', 'profesor']
+        fields = ['id', 'grupo', 'profesor', 'object_name']
 
 
 class GradoSerializer(serializers.ModelSerializer):
+    object_name = serializers.SerializerMethodField()
+
+    def get_object_name(self, obj):
+        return obj.__str__()
+
     class Meta:
         model = Grado
         fields = '__all__'
 
 
 class AsignaturaSerializer(serializers.ModelSerializer):
+    object_name = serializers.SerializerMethodField()
+
+    def get_object_name(self, obj):
+        return obj.__str__()
+
     class Meta:
         model = Asignatura
         fields = '__all__'
 
 
 class GrupoSerializer(serializers.ModelSerializer):
+    object_name = serializers.SerializerMethodField()
+
+    def get_object_name(self, obj):
+        return obj.__str__()
+
     class Meta:
         model = Grupo
         fields = '__all__'
 
 
 class NotaSerializer(serializers.ModelSerializer):
+    object_name = serializers.SerializerMethodField()
+
+    def get_object_name(self, obj):
+        return obj.__str__()
+
     class Meta:
         model = Nota
         fields = '__all__'
 
 
 class CursoSerializer(serializers.ModelSerializer):
+    object_name = serializers.SerializerMethodField()
+
+    def get_object_name(self, obj):
+        return obj.__str__()
+
     class Meta:
         model = Curso
         fields = '__all__'
 
 
 class ProgramaDeEstudioSerializer(serializers.ModelSerializer):
+    object_name = serializers.SerializerMethodField()
+
+    def get_object_name(self, obj):
+        return obj.__str__()
+
     class Meta:
         model = ProgramaDeEstudio
         fields = '__all__'
