6fdfa5efa4f43ec25e788558cbb73d1ec4cd7c13
Abel Cuza <abel.cuza07@gmail.com>
2022-11-20 18:52:44 -0500

    Update model fields and add __str__() function for all models

--- api/escuela/models.py
+++ api/escuela/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.functional import cached_property
 
 
 class Persona(models.Model):
@@ -8,7 +10,7 @@ class Persona(models.Model):
     ]
     nombre = models.CharField(max_length=30)
     primer_apellido = models.CharField(max_length=100)
-    segundo_apellido = models.CharField(max_length=100)
+    segundo_apellido = models.CharField(max_length=100, null=True, blank=True)
     fecha_nacimiento = models.DateField()
     ci = models.CharField(max_length=11, unique=True)
     direccion = models.CharField(max_length=200)
@@ -18,8 +20,13 @@ class Persona(models.Model):
     provincia = models.CharField(max_length=30)
     genero = models.CharField(max_length=1, choices=genero_choice)
 
+    @cached_property
     def edad(self):
-        return 18
+        if self.fecha_nacimiento:
+            today = timezone.now()
+            return today.year - self.fecha_nacimiento.year - (
+                    (today.month, today.day) < (self.fecha_nacimiento.month, self.fecha_nacimiento.day))
+        return None
 
     def __str__(self):
         return f'{self.nombre} {self.primer_apellido}'
@@ -29,26 +36,32 @@ class Persona(models.Model):
 
 
 class Cientifico(models.Model):
-    categoria_cientifica_choices = [
-        ('máster', 'máster'),
-        ('doctor', 'doctor'),
+    grado_academico_choices = [
+        ('universitario', 'Universitario'),
+        ('master', 'Máster en Ciencias'),
+        ('doctor', 'Doctor en Ciencias'),
     ]
-    categoria_cientifica = models.CharField(max_length=100, choices=categoria_cientifica_choices, null=True, blank=True)
+    grado_academico = models.CharField(max_length=100, choices=grado_academico_choices, null=True, blank=True)
 
     class Meta:
         abstract = True
 
 
 class Grado(models.Model):
+    GRADOS = {
+        '7': '7mo',
+        '8': '8vo',
+        '9': '9no'
+    }
     grado_choice = [
         ('7', '7mo'),
         ('8', '8vo'),
         ('9', '9no')
     ]
-    nombre = models.CharField(max_length=1, choices=grado_choice, primary_key=True)
+    nombre = models.CharField(max_length=1, choices=grado_choice)
 
     def __str__(self):
-        return self.nombre
+        return self.GRADOS[self.nombre]
 
 
 class Asignatura(models.Model):
@@ -60,30 +73,37 @@ class Asignatura(models.Model):
 
 class Grupo(models.Model):
     numero_grupo = models.CharField(max_length=30)
+    grado = models.ForeignKey(Grado, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.numero_grupo
+        return f'{self.grado.__str__()} {self.numero_grupo}'
+
+    class Meta:
+        unique_together = ('numero_grupo', 'grado')
 
 
 class Profesor(Persona, Cientifico):
     categoria_docente_choices = [
-        ('Profesor Titular', 'Profesor Titular'),
-        ('Profesor Auxiliar', 'Profesor Auxiliar'),
-        ('Asistente', 'Asistente'),
-        ('Instructor', 'Instructor'),
-        ('Instructor Auxiliar', 'Instructor Auxiliar'),
-        ('Auxiliar Técnico de la Docencia', 'Auxiliar Técnico de la Docencia'),
-        ('Profesor de Mérito', 'Profesor de Mérito'),
-        ('Profesor Invitado', 'Profesor Invitado')
+        ('profesor_titular', 'Profesor Titular'),
+        ('profesor_auxiliar', 'Profesor Auxiliar'),
+        ('asistente', 'Asistente'),
+        ('instructor', 'Instructor'),
+        ('instructor_auxiliar', 'Instructor Auxiliar'),
+        ('auxiliar_tecnico', 'Auxiliar Técnico de la Docencia'),
+        ('profesor_de_merito', 'Profesor de Mérito'),
+        ('profesor_invitado', 'Profesor Invitado')
     ]
     categoria_docente = models.CharField(max_length=100, choices=categoria_docente_choices, null=True, blank=True)
     grupos = models.ManyToManyField(Grupo)
-    asignatura = models.ManyToManyField(Asignatura)
+    asignaturas = models.ManyToManyField(Asignatura)
 
 
-class ProfesorGuia(Profesor):
+class ProfesorGuia(models.Model):
     grupo = models.OneToOneField(Grupo, on_delete=models.CASCADE)
     profesor = models.OneToOneField(Profesor, on_delete=models.CASCADE, related_name='pg_profesor')
+
+    def __str__(self):
+        return self.profesor.__str__()
 
 
 class Estudiante(Persona):
@@ -95,28 +115,31 @@ class Estudiante(Persona):
 
 class Nota(models.Model):
     tipo_choice = [
-        ('ES', 'Evaluación Sistemática'),
-        ('EP', 'Evaluación Parcial'),
-        ('EF', 'Evaluación Final')
+        ('es', 'Evaluación Sistemática'),
+        ('ep', 'Evaluación Parcial'),
+        ('ef', 'Evaluación Final')
     ]
     valor = models.IntegerField()
     tipo = models.CharField(max_length=100, choices=tipo_choice)
     estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
     asignatura = models.ForeignKey(Asignatura, on_delete=models.RESTRICT)
 
+    def __str__(self):
+        return self.valor
+
 
 class Curso(models.Model):
-    nombre = models.CharField(max_length=20, primary_key=True, unique=True)
+    nombre = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.nombre
 
 
 class ProgramaDeEstudio(models.Model):
-    asignatura = models.ManyToManyField(Asignatura)
+    asignaturas = models.ManyToManyField(Asignatura)
     curso = models.ForeignKey(Curso, on_delete=models.RESTRICT)
     grado = models.ForeignKey(Grado, on_delete=models.RESTRICT)
-    archivo = models.FileField(null=True)
+    archivo = models.FileField(null=True, upload_to='uploads/')
 
     def __str__(self):
         return f'{self.curso} {self.grado}'
